49d61fd77d68d3583fcdd19e18feaf2712e49d5b
Александр Рыжаков <rjizhakov@yandex.ru>
2026-04-27 14:25:58 +0500

    feat: add Spring Cloud infrastructure for cloud deployment

--- infra/gateway-server/src/main/java/ru/practicum/GatewayServer.java
+++ infra/gateway-server/src/main/java/ru/practicum/GatewayServer.java
@@ -2,10 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Main {
+@EnableDiscoveryClient
+public class GatewayServer {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 }
--- stats/stats-server/src/main/java/ru/practicum/StatsApp.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsApp.java
@@ -2,10 +2,12 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Main {
+@EnableDiscoveryClient
+public class StatsApp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(StatsApp.class, args);
     }
 }
